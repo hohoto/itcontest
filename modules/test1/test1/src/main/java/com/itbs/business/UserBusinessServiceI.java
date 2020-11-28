b2d305d63ef79e3598f4ad9bb2a789a2d2f3d930
shaoxuhang <sxh10463@163.com>
2020-11-28 16:59:48 +0800

    用户操作接口

---
+++ modules/test1/test1/src/main/java/com/itbs/business/UserBusinessServiceI.java
@@ -0,0 +1,7 @@
+package com.itbs.business;
+
+/**
+ * Created by shaoxuhang on 20/11/28.
+ */
+public interface UserBusinessServiceI {
+}
---
+++ modules/test1/test1/src/main/java/com/itbs/business/impl/UserBusinessService.java
@@ -0,0 +1,7 @@
+package com.itbs.business.impl;
+
+/**
+ * Created by shaoxuhang on 20/11/28.
+ */
+public class UserBusinessService {
+}
