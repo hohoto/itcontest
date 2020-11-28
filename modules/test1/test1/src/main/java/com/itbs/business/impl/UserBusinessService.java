1e3a8fab5f3718d146ea34ed609cd8f7caab4cd8
shaoxuhang <sxh10463@163.com>
2020-11-28 17:46:52 +0800

    dao

--- modules/test1/test1/src/main/java/com/itbs/business/impl/UserBusinessService.java
+++ modules/test1/test1/src/main/java/com/itbs/business/impl/UserBusinessService.java
@@ -1,7 +1,31 @@
 package com.itbs.business.impl;
 
+import com.itbs.business.UserBusinessServiceI;
+import com.itbs.persistence.entity.TBasicCompany;
+import com.itbs.persistence.entity.TBasicCompanyExample;
+import com.itbs.service.ItMatchDaoServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by shaoxuhang on 20/11/28.
  */
-public class UserBusinessService {
+@Component
+public class UserBusinessService implements UserBusinessServiceI{
+
+    @Autowired
+    ItMatchDaoServiceI itMatchDaoServiceI;
+
+    @Override
+    public List<TBasicCompany> queryCompany(String companyCode){
+        TBasicCompanyExample example = new TBasicCompanyExample();
+        TBasicCompanyExample.Criteria criteria = example.createCriteria();
+        criteria.andCompanyCodeEqualTo(companyCode);
+        List<TBasicCompany> tBasicCompanies = itMatchDaoServiceI.selectTBasicCompany(example);
+        return tBasicCompanies;
+    }
+
+
 }
