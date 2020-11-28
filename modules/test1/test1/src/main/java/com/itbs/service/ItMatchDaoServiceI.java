4818b6f2427acf8dc0c1f9d0b933d1be53afbd93
shaoxuhang <sxh10463@163.com>
2020-11-28 17:44:15 +0800

    dao

---
+++ modules/test1/test1/src/main/java/com/itbs/service/ItMatchDaoServiceI.java
@@ -0,0 +1,7 @@
+package com.itbs.service;
+
+/**
+ * Created by shaoxuhang on 20/11/28.
+ */
+public interface ItMatchDaoServiceImpl {
+}
---
+++ modules/test1/test1/src/main/java/com/itbs/service/impl/ItMatchDaoServiceImpl.java
@@ -0,0 +1,7 @@
+package com.itbs.service.impl;
+
+/**
+ * Created by shaoxuhang on 20/11/28.
+ */
+public class ItMatchDaoServiceImpl {
+}
