78d7d62f801596fff33bcabbd284c5b089272864
gzsll <894521464@qq.com>
2020-11-28 18:00:29 +0800

    入库

--- modules/test2/test2/src/main/java/com/itbs/service/ApprovalService.java
+++ modules/test2/test2/src/main/java/com/itbs/service/ApprovalService.java
@@ -1,10 +1,10 @@
 package com.itbs.service;
 
 import com.itbs.api.ApprovalManage;
-import com.itbs.commend.*;
+import com.itbs.persistence.entity.Resourcemanage;
+import com.itbs.persistence.mapper.ResourcemanageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,40 +12,48 @@ import java.util.Map;
 @Service
 public class ApprovalService implements ApprovalManage {
 
+    @Autowired
+    ResourcemanageMapper resourcemanageMapper;
+
+
     @Override
-    public List<ResourceCommend> AllApproval(){
-        List<ResourceCommend> resourceCommendList = new ArrayList<>();
+    public List<Resourcemanage> AllApproval(){
+        List<Resourcemanage> resourceCommendList = resourcemanageMapper.selectResStaEquThr();
         return resourceCommendList;
     }
 
     @Override
-    public Map<String,String> ShenlingApproval(ResourceCommend resourceCommend){
+    public Map<String,String> ShenlingApproval(Resourcemanage resourcemanage){
         Map mp = new HashMap();
 
+        mp.put("resource","31");
         mp.put("status","1");
         return mp;
     }
 
     @Override
-    public Map<String,String> JieyongApproval(ResourceCommend resourceCommend){
+    public Map<String,String> JieyongApproval(Resourcemanage resourcemanage){
         Map mp = new HashMap();
 
+        mp.put("resource","32");
         mp.put("status","1");
         return mp;
     }
 
     @Override
-    public Map<String,String> BiangengApproval(ResourceCommend resourceCommend){
+    public Map<String,String> BiangengApproval(Resourcemanage resourcemanage){
         Map mp = new HashMap();
 
+        mp.put("resource","34");
         mp.put("status","1");
         return mp;
     }
 
     @Override
-    public Map<String,String> BaofeiApproval(ResourceCommend resourceCommend){
+    public Map<String,String> BaofeiApproval(Resourcemanage resourcemanage){
         Map mp = new HashMap();
 
+        mp.put("resource","51");
         mp.put("status","1");
         return mp;
     }
